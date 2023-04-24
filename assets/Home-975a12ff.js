c29b4b5dcdfc209438eaba672aada69c73410365
Petro-Psekho <Petro-Psekho@users.noreply.github.com>
2023-04-24 07:01:58 +0000

    deploy: 5e489c7f02f2f38a3679eddd9260019282056c9c

---
+++ assets/Home-975a12ff.js
@@ -0,0 +1,10 @@
+import{n as e,j as t}from"./index-491eccdb.js";const s="/goit-test-task/assets/collage-6eb88c81.png",o=e.div`
+  display: flex;
+
+  justify-content: center;
+
+  padding: 8px 0;
+  margin-top: 36px;
+`,a=e.img`
+  width: 900px;
+`;function n(){return t.jsx(o,{children:t.jsx(a,{src:s,alt:"collage"})})}export{n as default};
