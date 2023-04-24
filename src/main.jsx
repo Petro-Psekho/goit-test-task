9f35a726ae53b8bf3646121db7881f716040aecf
Petro Psekho <psekho.pp@gmail.com>
2023-04-24 14:46:24 +0300

    update HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename="/goit-test-task/">
+    <HashRouter>
       <App />
     </HashRouter>
     {/* <BrowserRouter basename="/goit-test-task/">
