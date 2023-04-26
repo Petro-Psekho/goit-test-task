c7f631d33751ff0e3773f06869afa9ec8d45d58d
Petro-Psekho <Petro-Psekho@users.noreply.github.com>
2023-04-26 06:43:13 +0000

    deploy: f0caa906e16710764a3213df855885687552a932

--- assets/Cards-e720723c.js
+++ assets/Cards-e720723c.js
@@ -1,4 +1,4 @@
-import{h as xi,r as E,E as Ai,c as Fi,s as Pi,_ as R,a as Di,R as ee,i as Rn,n as ae,j as J,L as Ti,u as Ri,Q as Ii}from"./index-865b6a00.js";var Ye={},_i={get exports(){return Ye},set exports(t){Ye=t}},Y={};/** @license React v16.13.1
+import{h as xi,r as E,E as Ai,c as Fi,s as Pi,_ as R,a as Di,R as ee,i as Rn,n as ae,j as J,L as Ti,u as Ri,Q as Ii}from"./index-f5c87522.js";var Ye={},_i={get exports(){return Ye},set exports(t){Ye=t}},Y={};/** @license React v16.13.1
  * react-is.production.min.js
  *
  * Copyright (c) Facebook, Inc. and its affiliates.
---
+++ assets/Home-1ebcdb5c.js
@@ -0,0 +1,8 @@
+import{n as e,j as t}from"./index-f5c87522.js";const s="/goit-test-task/assets/collage-6eb88c81.png",l=e.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`,n=e.img`
+  width: 900px;
+`;function a(){return t.jsx(l,{children:t.jsx(n,{src:s,alt:"collage"})})}export{a as default};
